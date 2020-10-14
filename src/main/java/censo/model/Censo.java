5e19b2be788bd90a67ee9e92c257bbc2e0ff8467
Henrique Lustosa <henriqlustosa@gmail.com>
2020-10-14 16:34:43 -0300

    Finalizado o Produto - API para o Sistema de Isolamento

--- src/main/java/censo/model/Censo.java
+++ src/main/java/censo/model/Censo.java
@@ -9,32 +9,22 @@ import censo.model.Censo;
 public class Censo {
 	private String cd_prontuario;
 	private String nm_paciente;
+	private String dt_nascimento;
+	private String nr_quarto;
+	private String dt_internacao_data;
+	private String dt_internacao_hora;
+	private String nm_clinica;
 	private String in_sexo;
 	private Integer nr_idade;
-	private String nr_quarto;
-	private String nr_leito;
-	private String nm_ala;
-	private String nm_clinica;
+	private String cod_CID;
+	private String descricaoCID;
 	private String nm_unidade_funcional;
-	private String nm_acomodacao;
-	private String st_leito;
-	private String  dt_internacao;
-	private String  dt_entrada_setor;
-	private String nm_especialidade;
-	private String nm_medico;
-	private String dt_ultimo_evento;
-	private String nm_origem;
-	private String sg_cid;
-	private String tx_observacao;
-	private Integer nr_convenio;
-	private Integer nr_plano;
-	private String nm_convenio_plano;
-	private String nr_crm_profissional;
-	private String nm_carater_internacao;
-	private String nm_origem_internacao;
-	private String nr_procedimento;
-	private String  dt_alta_medica;
-	private String  dt_saida_paciente;
+	private String tempo;
+	private String vinculo;
+	
+
+
+	
 	public String getCd_prontuario() {
 		return cd_prontuario;
 	}
@@ -65,179 +55,51 @@ public class Censo {
 	public void setNr_quarto(String nr_quarto) {
 		this.nr_quarto = nr_quarto;
 	}
-	public String getNr_leito() {
-		return nr_leito;
-	}
-	public void setNr_leito(String nr_leito) {
-		this.nr_leito = nr_leito;
-	}
-	public String getNm_ala() {
-		return nm_ala;
-	}
-	public void setNm_ala(String nm_ala) {
-		this.nm_ala = nm_ala;
-	}
+
 	public String getNm_clinica() {
 		return nm_clinica;
 	}
 	public void setNm_clinica(String nm_clinica) {
 		this.nm_clinica = nm_clinica;
 	}
-	public String getNm_unidade_funcional() {
-		return nm_unidade_funcional;
-	}
-	public void setNm_unidade_funcional(String nm_unidade_funcional) {
-		this.nm_unidade_funcional = nm_unidade_funcional;
-	}
-	public String getNm_acomodacao() {
-		return nm_acomodacao;
-	}
-	public void setNm_acomodacao(String nm_acomodacao) {
-		this.nm_acomodacao = nm_acomodacao;
-	}
-	public String getSt_leito() {
-		return st_leito;
-	}
-	public void setSt_leito(String st_leito) {
-		this.st_leito = st_leito;
-	}
-	public String  getDt_internacao() {
-		return dt_internacao;
-	}
-	public void setDt_internacao(String  dt_internacao) {
-		this.dt_internacao = dt_internacao;
-	}
-	public String  getDt_entrada_setor() {
-		return dt_entrada_setor;
-	}
-	public void setDt_entrada_setor(String  dt_entrada_setor) {
-		this.dt_entrada_setor = dt_entrada_setor;
-	}
-	public String getNm_especialidade() {
-		return nm_especialidade;
-	}
-	public void setNm_especialidade(String nm_especialidade) {
-		this.nm_especialidade = nm_especialidade;
-	}
-	public String getNm_medico() {
-		return nm_medico;
-	}
-	public void setNm_medico(String nm_medico) {
-		this.nm_medico = nm_medico;
-	}
-	public String  getDt_ultimo_evento() {
-		return dt_ultimo_evento;
-	}
-	public void setDt_ultimo_evento(String  dt_ultimo_evento2) {
-		this.dt_ultimo_evento = dt_ultimo_evento2;
-	}
-	public String getNm_origem() {
-		return nm_origem;
-	}
-	public void setNm_origem(String nm_origem) {
-		this.nm_origem = nm_origem;
-	}
-	
 
-	public String getSg_cid() {
-		return sg_cid;
-	}
-	public void setSg_cid(String sg_cid) {
-		this.sg_cid = sg_cid;
-	}
-	public String getTx_observacao() {
-		return tx_observacao;
-	}
-	public void setTx_observacao(String tx_observacao) {
-		this.tx_observacao = tx_observacao;
-	}
-	public Integer getNr_convenio() {
-		return nr_convenio;
-	}
-	public void setNr_convenio(Integer nr_convenio) {
-		this.nr_convenio = nr_convenio;
-	}
-	public Integer getNr_plano() {
-		return nr_plano;
-	}
-	public void setNr_plano(Integer nr_plano) {
-		this.nr_plano = nr_plano;
-	}
-	public String getNm_convenio_plano() {
-		return nm_convenio_plano;
-	}
-	public void setNm_convenio_plano(String nm_convenio_plano) {
-		this.nm_convenio_plano = nm_convenio_plano;
-	}
-	public String getNr_crm_profissional() {
-		return nr_crm_profissional;
-	}
-	public void setNr_crm_profissional(String nr_crm_profissional) {
-		this.nr_crm_profissional = nr_crm_profissional;
-	}
-	public String getNm_carater_internacao() {
-		return nm_carater_internacao;
-	}
-	public void setNm_carater_internacao(String nm_carater_internacao) {
-		this.nm_carater_internacao = nm_carater_internacao;
-	}
-	public String getNm_origem_internacao() {
-		return nm_origem_internacao;
-	}
-	public void setNm_origem_internacao(String nm_origem_internacao) {
-		this.nm_origem_internacao = nm_origem_internacao;
-	}
-	public String getNr_procedimento() {
-		return nr_procedimento;
-	}
-	public void setNr_procedimento(String nr_procedimento) {
-		this.nr_procedimento = nr_procedimento;
-	}
-	public String  getDt_alta_medica() {
-		return dt_alta_medica;
-	}
-	public void setDt_alta_medica(String  dt_alta_medica) {
-		this.dt_alta_medica = dt_alta_medica;
-	}
-	public String getDt_saida_paciente() {
-		return dt_saida_paciente;
-	}
-	public void setDt_saida_paciente(String  dt_saida_paciente) {
-		this.dt_saida_paciente = dt_saida_paciente;
-	}
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((cd_prontuario == null) ? 0 : cd_prontuario.hashCode());
-		result = prime * result + ((dt_alta_medica == null) ? 0 : dt_alta_medica.hashCode());
-		result = prime * result + ((dt_entrada_setor == null) ? 0 : dt_entrada_setor.hashCode());
-		result = prime * result + ((dt_internacao == null) ? 0 : dt_internacao.hashCode());
-		result = prime * result + ((dt_saida_paciente == null) ? 0 : dt_saida_paciente.hashCode());
-		result = prime * result + ((dt_ultimo_evento == null) ? 0 : dt_ultimo_evento.hashCode());
-		result = prime * result + ((in_sexo == null) ? 0 : in_sexo.hashCode());
-		result = prime * result + ((nm_acomodacao == null) ? 0 : nm_acomodacao.hashCode());
-		result = prime * result + ((nm_ala == null) ? 0 : nm_ala.hashCode());
-		result = prime * result + ((nm_carater_internacao == null) ? 0 : nm_carater_internacao.hashCode());
-		result = prime * result + ((nm_clinica == null) ? 0 : nm_clinica.hashCode());
-		result = prime * result + ((nm_convenio_plano == null) ? 0 : nm_convenio_plano.hashCode());
-		result = prime * result + ((nm_especialidade == null) ? 0 : nm_especialidade.hashCode());
-		result = prime * result + ((nm_medico == null) ? 0 : nm_medico.hashCode());
-		result = prime * result + ((nm_origem == null) ? 0 : nm_origem.hashCode());
-		result = prime * result + ((nm_origem_internacao == null) ? 0 : nm_origem_internacao.hashCode());
+		
 		result = prime * result + ((nm_paciente == null) ? 0 : nm_paciente.hashCode());
-		result = prime * result + ((nm_unidade_funcional == null) ? 0 : nm_unidade_funcional.hashCode());
-		result = prime * result + ((nr_convenio == null) ? 0 : nr_convenio.hashCode());
-		result = prime * result + ((nr_crm_profissional == null) ? 0 : nr_crm_profissional.hashCode());
-		result = prime * result + ((nr_idade == null) ? 0 : nr_idade.hashCode());
-		result = prime * result + ((nr_leito == null) ? 0 : nr_leito.hashCode());
-		result = prime * result + ((nr_plano == null) ? 0 : nr_plano.hashCode());
-		result = prime * result + ((nr_procedimento == null) ? 0 : nr_procedimento.hashCode());
+		
+		result = prime * result + ((dt_nascimento == null) ? 0 : dt_nascimento.hashCode());
+		
 		result = prime * result + ((nr_quarto == null) ? 0 : nr_quarto.hashCode());
 		
-		result = prime * result + ((sg_cid == null) ? 0 : sg_cid.hashCode());
-		result = prime * result + ((st_leito == null) ? 0 : st_leito.hashCode());
-		result = prime * result + ((tx_observacao == null) ? 0 : tx_observacao.hashCode());
+		result = prime * result + ((dt_internacao_data == null) ? 0 : dt_internacao_data.hashCode());
+		
+		result = prime * result + ((dt_internacao_hora == null) ? 0 : dt_internacao_hora.hashCode());
+		
+		result = prime * result + ((nm_clinica == null) ? 0 : nm_clinica.hashCode());
+	
+		result = prime * result + ((in_sexo == null) ? 0 : in_sexo.hashCode());
+		
+		result = prime * result + ((nr_idade == null) ? 0 : nr_idade.hashCode());
+		
+		result = prime * result + ((cod_CID == null) ? 0 : cod_CID.hashCode());
+		
+		result = prime * result + ((descricaoCID == null) ? 0 : descricaoCID.hashCode());
+		
+		result = prime * result + ((nm_unidade_funcional == null) ? 0 : nm_unidade_funcional.hashCode());
+		
+		result = prime * result + ((tempo == null) ? 0 : tempo.hashCode());
+		
+		result = prime * result + ((vinculo == null) ? 0 : vinculo.hashCode());
+	
+		
+		
+	
 		return result;
 	}
 	@Override
@@ -254,143 +116,143 @@ public class Censo {
 				return false;
 		} else if (!cd_prontuario.equals(other.cd_prontuario))
 			return false;
-		if (dt_alta_medica == null) {
-			if (other.dt_alta_medica != null)
-				return false;
-		} else if (!dt_alta_medica.equals(other.dt_alta_medica))
-			return false;
-		if (dt_entrada_setor == null) {
-			if (other.dt_entrada_setor != null)
-				return false;
-		} else if (!dt_entrada_setor.equals(other.dt_entrada_setor))
-			return false;
-		if (dt_internacao == null) {
-			if (other.dt_internacao != null)
-				return false;
-		} else if (!dt_internacao.equals(other.dt_internacao))
-			return false;
-		if (dt_saida_paciente == null) {
-			if (other.dt_saida_paciente != null)
-				return false;
-		} else if (!dt_saida_paciente.equals(other.dt_saida_paciente))
-			return false;
-		if (dt_ultimo_evento == null) {
-			if (other.dt_ultimo_evento != null)
-				return false;
-		} else if (!dt_ultimo_evento.equals(other.dt_ultimo_evento))
-			return false;
-		if (in_sexo == null) {
-			if (other.in_sexo != null)
-				return false;
-		} else if (!in_sexo.equals(other.in_sexo))
-			return false;
-		if (nm_acomodacao == null) {
-			if (other.nm_acomodacao != null)
-				return false;
-		} else if (!nm_acomodacao.equals(other.nm_acomodacao))
-			return false;
-		if (nm_ala == null) {
-			if (other.nm_ala != null)
-				return false;
-		} else if (!nm_ala.equals(other.nm_ala))
-			return false;
-		if (nm_carater_internacao == null) {
-			if (other.nm_carater_internacao != null)
-				return false;
-		} else if (!nm_carater_internacao.equals(other.nm_carater_internacao))
-			return false;
-		if (nm_clinica == null) {
-			if (other.nm_clinica != null)
-				return false;
-		} else if (!nm_clinica.equals(other.nm_clinica))
-			return false;
-		if (nm_convenio_plano == null) {
-			if (other.nm_convenio_plano != null)
-				return false;
-		} else if (!nm_convenio_plano.equals(other.nm_convenio_plano))
-			return false;
-		if (nm_especialidade == null) {
-			if (other.nm_especialidade != null)
-				return false;
-		} else if (!nm_especialidade.equals(other.nm_especialidade))
-			return false;
-		if (nm_medico == null) {
-			if (other.nm_medico != null)
-				return false;
-		} else if (!nm_medico.equals(other.nm_medico))
-			return false;
-		if (nm_origem == null) {
-			if (other.nm_origem != null)
-				return false;
-		} else if (!nm_origem.equals(other.nm_origem))
-			return false;
-		if (nm_origem_internacao == null) {
-			if (other.nm_origem_internacao != null)
-				return false;
-		} else if (!nm_origem_internacao.equals(other.nm_origem_internacao))
-			return false;
+		
 		if (nm_paciente == null) {
 			if (other.nm_paciente != null)
 				return false;
 		} else if (!nm_paciente.equals(other.nm_paciente))
 			return false;
-		if (nm_unidade_funcional == null) {
-			if (other.nm_unidade_funcional != null)
+		
+		if (dt_nascimento == null) {
+			if (other.dt_nascimento != null)
 				return false;
-		} else if (!nm_unidade_funcional.equals(other.nm_unidade_funcional))
+		} else if (!dt_nascimento.equals(other.dt_nascimento))
 			return false;
-		if (nr_convenio == null) {
-			if (other.nr_convenio != null)
-				return false;
-		} else if (!nr_convenio.equals(other.nr_convenio))
-			return false;
-		if (nr_crm_profissional == null) {
-			if (other.nr_crm_profissional != null)
-				return false;
-		} else if (!nr_crm_profissional.equals(other.nr_crm_profissional))
-			return false;
-		if (nr_idade == null) {
-			if (other.nr_idade != null)
-				return false;
-		} else if (!nr_idade.equals(other.nr_idade))
-			return false;
-		if (nr_leito == null) {
-			if (other.nr_leito != null)
-				return false;
-		} else if (!nr_leito.equals(other.nr_leito))
-			return false;
-		if (nr_plano == null) {
-			if (other.nr_plano != null)
-				return false;
-		} else if (!nr_plano.equals(other.nr_plano))
-			return false;
-		if (nr_procedimento == null) {
-			if (other.nr_procedimento != null)
-				return false;
-		} else if (!nr_procedimento.equals(other.nr_procedimento))
-			return false;
+		
+
 		if (nr_quarto == null) {
 			if (other.nr_quarto != null)
 				return false;
 		} else if (!nr_quarto.equals(other.nr_quarto))
 			return false;
+		
+		
+		if (dt_internacao_data == null) {
+			if (other.dt_internacao_data != null)
+				return false;
+		} else if (!dt_internacao_data.equals(other.dt_internacao_data))
+			return false;
+		if (dt_internacao_hora == null) {
+			if (other.dt_internacao_hora != null)
+				return false;
+		} else if (!dt_internacao_hora.equals(other.dt_internacao_hora))
+			return false;
+
+		if (nm_clinica == null) {
+			if (other.nm_clinica != null)
+				return false;
+		} else if (!nm_clinica.equals(other.nm_clinica))
+			return false;
+		
+		
+		if (in_sexo == null) {
+			if (other.in_sexo != null)
+				return false;
+		} else if (!in_sexo.equals(other.in_sexo))
+			return false;
+		
+		if (nr_idade == null) {
+			if (other.nr_idade != null)
+				return false;
+		} else if (!nr_idade.equals(other.nr_idade))
+			return false;
 	
-		if (sg_cid == null) {
-			if (other.sg_cid != null)
+		if (cod_CID == null) {
+			if (other.cod_CID != null)
 				return false;
-		} else if (!sg_cid.equals(other.sg_cid))
+		} else if (!cod_CID.equals(other.cod_CID))
 			return false;
-		if (st_leito == null) {
-			if (other.st_leito != null)
+		
+		if (descricaoCID == null) {
+			if (other.descricaoCID != null)
 				return false;
-		} else if (!st_leito.equals(other.st_leito))
+		} else if (!cod_CID.equals(other.descricaoCID))
 			return false;
-		if (tx_observacao == null) {
-			if (other.tx_observacao != null)
+		
+		if (nm_unidade_funcional == null) {
+			if (other.nm_unidade_funcional != null)
 				return false;
-		} else if (!tx_observacao.equals(other.tx_observacao))
+		} else if (!nm_unidade_funcional.equals(other.nm_unidade_funcional))
 			return false;
+		
+		if (tempo == null) {
+			if (other.tempo != null)
+				return false;
+		} else if (!tempo.equals(other.tempo))
+			return false;
+	
+		
+		if (vinculo == null) {
+			if (other.vinculo != null)
+				return false;
+		} else if (!vinculo.equals(other.vinculo))
+			return false;
+	
+	
+		
 		return true;
+	}
+	public String getDt_nascimento() {
+		return dt_nascimento;
+	}
+	public void setDt_nascimento(String dt_nascimento) {
+		this.dt_nascimento = dt_nascimento;
+	}
+	
+	public String getCod_CID() {
+		return cod_CID;
+	}
+	public void setCod_CID(String cod_CID) {
+		this.cod_CID = cod_CID;
+	}
+	public String getDescricaoCID() {
+		return descricaoCID;
+	}
+	public void setDescricaoCID(String descricaoCID) {
+		this.descricaoCID = descricaoCID;
+	}
+	public String getVinculo() {
+		return vinculo;
+	}
+	public void setVinculo(String vinculo) {
+		this.vinculo = vinculo;
+	}
+	public String getNm_unidade_funcional() {
+		return nm_unidade_funcional;
+	}
+	public void setNm_unidade_funcional(String nm_unidade_funcional) {
+		this.nm_unidade_funcional = nm_unidade_funcional;
+	}
+	public String getDt_internacao_data() {
+		return dt_internacao_data;
+	}
+
+	
+	public String getTempo() {
+		return tempo;
+	}
+	public void setTempo(String tempo) {
+		this.tempo = tempo;
+	}
+
+	public void setDt_internacao_data(String dt_internacao_data) {
+		this.dt_internacao_data = dt_internacao_data;
+	}
+	public String getDt_internacao_hora() {
+		return dt_internacao_hora;
+	}
+	public void setDt_internacao_hora(String dt_internacao_hora) {
+		this.dt_internacao_hora = dt_internacao_hora;
 	}
 
 
